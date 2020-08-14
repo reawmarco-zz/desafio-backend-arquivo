e8d6f5fadb1354ab24e46c807ac23fb1b48e4b19
Marco Oliveira <marco.141992@gmail.com>
2020-08-14 08:28:17 -0300

    package model. Model class

---
+++ src/main/java/com/salesman/model/Sale.java
@@ -0,0 +1,11 @@
+package com.salesman.model;
+
+import java.util.List;
+
+public class Sale {
+
+    private List<SaleItem> saleItems;
+    private Salesman salesman;
+    private String salesmanName;
+
+}
---
+++ src/main/java/com/salesman/model/SaleItem.java
@@ -0,0 +1,38 @@
+package com.salesman.model;
+
+public class SaleItem {
+
+    private long id;
+    private double quantity;
+    private double price;
+
+    public SaleItem(long id, double quantity, double price) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
