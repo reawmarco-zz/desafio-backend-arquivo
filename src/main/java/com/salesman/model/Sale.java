91843e7eb364cffcb77f0dc71cf13bb12c59ac0a
Marco Oliveira <marco.141992@gmail.com>
2020-08-14 08:29:11 -0300

    package model. Model class

--- src/main/java/com/salesman/model/Sale.java
+++ src/main/java/com/salesman/model/Sale.java
@@ -8,4 +8,28 @@ public class Sale {
     private Salesman salesman;
     private String salesmanName;
 
+
+    public List<SaleItem> getSaleItems() {
+        return saleItems;
+    }
+
+    public void setSaleItems(List<SaleItem> saleItems) {
+        this.saleItems = saleItems;
+    }
+
+    public Salesman getSalesman() {
+        return salesman;
+    }
+
+    public void setSalesman(Salesman salesman) {
+        this.salesman = salesman;
+    }
+
+    public String getSalesmanName() {
+        return salesmanName;
+    }
+
+    public void setSalesmanName(String salesmanName) {
+        this.salesmanName = salesmanName;
+    }
 }
